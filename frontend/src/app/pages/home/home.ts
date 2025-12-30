5d2a1d42b01c83bf4d5a6105c15012a40bed9118
NejlaSilajdzija <nejlasilajdzija1@gmail.com>
2025-12-30 19:59:29 +0100

    Add responsive design using Angular Material for header and home

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {Router} from '@angular/router';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {map, Observable, shareReplay} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,16 @@ import {Router} from '@angular/router';
   styleUrl: './home.scss',
 })
 export class Home {
+  private breakpointObserver = inject(BreakpointObserver);
+
   constructor(private router: Router) {}
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map(result=> result.matches),
+      shareReplay()
+    );
 
   goToShop() {
     this.router.navigate(['/shop']);
